58b6d7fa6c70e7687c5c6e996cd19b69d86b135e
Barakat Bolaji <barakatbolaji65@gmail.com>
2023-04-14 14:57:07 +0100

    - implementing social links - decorating social links -create about page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />;
+      <Home />;
+      <About />;
+      <SocialLinks />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div>
+        <div>
+          <p>About</p>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora optio
+          totam iste voluptatem cumque vel, eaque voluptatum eligendi omnis, aut
+          suscipit corrupti, illo ducimus beatae nulla magnam et. Voluptate,
+          nobis voluptates. Similique quae quam, sunt consequuntur earum facere
+          eius iure voluptate sint molestias recusandae amet aliquid, asperiores
+          natus doloribus dolore?
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ex.
+          Doloribus nemo molestiae nostrum harum placeat ut tempore quidem
+          perspiciatis, consequatur possimus rerum, ea, autem accusantium
+          quibusdam nobis voluptatum! Odit recusandae esse laboriosam deserunt
+          hic repellat iste officiis doloremque quod temporibus, sint in eveniet
+          tempore, unde nostrum nisi beatae ad?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
