fecfd48ff487a777bde52009e99cd940c830c646
Barakat Bolaji <barakatbolaji65@gmail.com>
2023-04-14 15:15:07 +0100

    - decorating the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,13 @@ const About = () => {
       name="about"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div>
-        <div>
-          <p>About</p>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
-        <p>
+        <p className="text-xl mt-20">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora optio
           totam iste voluptatem cumque vel, eaque voluptatum eligendi omnis, aut
           suscipit corrupti, illo ducimus beatae nulla magnam et. Voluptate,
@@ -19,7 +21,7 @@ const About = () => {
           natus doloribus dolore?
         </p>
         <br />
-        <p>
+        <p className="text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ex.
           Doloribus nemo molestiae nostrum harum placeat ut tempore quidem
           perspiciatis, consequatur possimus rerum, ea, autem accusantium
