b833fb33e04c47334dddc9ce0ef6ad36c5b418ac
Barakat Bolaji <barakatbolaji65@gmail.com>
2023-04-15 11:14:23 +0100

    - implementing experience page and styling

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -52,7 +52,7 @@ const Portfolio = () => {
               <img
                 src={src}
                 alt=""
-                className="rounded-md duration-200 hover:scale-150"
+                className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-0 m-4 hover:scale-105 duration-200">
